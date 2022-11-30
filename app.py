9098ec7f812a8dc97c3b432b24e4e06c820dc273
felipe-paes <felipe2013.paes@gmail.com>
2022-11-30 20:56:04 +0000

    API

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import urllib.request, json
+
 app = Flask(__name__)
 
 frutas = []
@@ -22,5 +24,28 @@ def sobre():
 
 	return render_template("sobre.html", registros=registros)
 
-app.run(debug=True)
-#http://127.0.0.1:5000
+@app.route('/filmes/<propriedade>')
+def filmes(propriedade):
+
+	if propriedade == 'populares':
+		url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3ddc9b92db4de6c6559569c67bd88a13"
+	elif propriedade == 'kids':
+		url = "https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=3ddc9b92db4de6c6559569c67bd88a13"
+	elif propriedade == '2010':
+		url = "https://api.themoviedb.org/3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&api_key=3ddc9b92db4de6c6559569c67bd88a13"
+	elif propriedade == 'drama':
+		url = "https://api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10&api_key=3ddc9b92db4de6c6559569c67bd88a13"
+	elif propriedade == 'tom_cruise':
+		url = "https://api.themoviedb.org/3/discover/movie?with_genres=878&with_cast=500&sort_by=vote_average.desc&api_key=3ddc9b92db4de6c6559569c67bd88a13"
+
+	resposta = urllib.request.urlopen(url)
+
+	dados = resposta.read()
+
+	jsondata = json.loads(dados)
+
+	return render_template("filmes.html", filmes=jsondata['results'])
+
+
+if __name__ =="__main__":
+	app.run(debug=True)
